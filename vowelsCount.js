0d35643f8a47af5603c9dc790fa1c4b4014f1e14
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2019-03-27 12:23:29 -0400

    another kata

--- vowelsCount.js
+++ vowelsCount.js
@@ -1,6 +1,6 @@
-function getCount(str) {
-    var vowelsCount = 0;
-    var string = str.toString();
+getCount = (str) => {
+    let vowelsCount = 0;
+    let string = str.toString();
 
     for (let i = 0; i < string.length; i++) {
         if ((string.charAt(i) == 'a') || (string.charAt(i) == 'e') || (string.charAt(i) == 'i') || (string.charAt(i) == 'o') || (string.charAt(i) == 'u')) {
